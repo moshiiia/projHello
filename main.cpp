98bb2030860e8f3a326aafd224ad14ce08505df2
maria <mivk17@gmail.com>
2024-12-07 10:32:32 +0700

    Fix3

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
     cout<< "Hello. It`s first string"<<endl;
-    cout<< "Hello. It`s second string"<<endl;
+    cout<< "Hello. It`s second ..."<<endl;
+
     return 0;
 }
